b4366b8d550c7624c7b9b77cc1bd428699dbdcdb
sora-coder <rqqgx271@yahoo.co.jp>
2021-02-23 15:57:29 +0900

    sqlの実行

--- index.php
+++ index.php
@@ -24,9 +24,11 @@ try {
 } catch(PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
 }
+
+$count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰,ピンク,甘い"');
+echo $count . '件のデータを挿入しました';
+
 ?>
-
-
 </pre>
 </main>
 </body>
