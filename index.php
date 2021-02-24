7e436aa2c5b62c3f69a3abdf97c26ae2dd3ae72b
sora-coder <rqqgx271@yahoo.co.jp>
2021-02-24 10:57:47 +0900

    50文字だけ表示させる

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
 
 <article>
     <?php while ($memo = $memos->fetch()): ?>
-        <p><a href="#"><?php print($memo['memo']); ?></a></p>
+        <p><a href="#"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
         <time><?php print($memo['created_at']); ?></time>
         <hr>
     <?php endwhile; ?>
