ce4889ba7b7c3e3e9b44e83981da2ba55218175b
sora-coder <rqqgx271@yahoo.co.jp>
2021-02-24 15:55:35 +0900

    ページネーションのリンク作成

--- index.php
+++ index.php
@@ -41,11 +41,17 @@
     <?php endwhile; ?>
 
     <?php if($page >= 2): ?>
-    <a href="index.php?page=<?php print($page - 1); ?>"><?php print($page - 1); ?>ページ目へ</a>
+        <a href="index.php?page=<?php print($page - 1); ?>"><?php print($page - 1); ?>ページ目へ</a>
     <?php endif; ?>
     |
+    <?php
+        $counts = $db->query('SELECT COUNT(*) AS cnt FROM memos');
+        $count = $counts->fetch();
+        $max_page = ceil($count['cnt'] / 5);
+        if($page < $max_page):
+    ?>
     <a href="index.php?page=<?php print($page + 1); ?>"><?php print($page + 1); ?>ページ目へ</a>
-
+    <?php endif; ?>
 </article>
 </main>
 </body>
